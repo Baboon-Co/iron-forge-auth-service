a638704348c6b06bc575ea2016673cbca959c455
Nikita Smolianitski <n.smolianitski@gmail.com>
2025-04-15 22:52:59 +0300

    Initial commit

---
+++ src/Web/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    public IActionResult Register()
+    {
+        return Ok();
+    }
+
+    public IActionResult Login()
+    {
+        return Ok();
+    }
+}
